73cd559443d991382c5b29f90d6920ea113854af
Danny Manzietti <danmanzietti@gmail.com>
2019-10-07 17:45:37 -0400

    finished mvp and one stretch of being able to remove items from cart

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+
+import { ProductContext } from "./contexts/ProductContext";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
 
-	const addItem = item => {
-		setCart([...cart, item]);
-	};
+  const [cart, setCart] = useState(() => {
+    const item = window.localStorage.getItem("user");
+    return item ? JSON.parse(item) : [];
+  });
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  useEffect(() => {
+    window.localStorage.setItem("user", JSON.stringify(cart));
+  }, [cart]);
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+  const addItem = item => {
+    setCart([...cart, item]);
+    console.log("Picked item is: ", item);
+  };
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+  const removeItem = item => {
+    setCart([...cart].filter(num => num.id !== item.id));
+  };
+
+  return (
+    <div className="App">
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{ cart, removeItem }}>
+          <Navigation cart={cart} />
+
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
